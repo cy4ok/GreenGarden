174627309afe7b812f44074954bee2ca492915d3
cy4ok <cy4ok86@gmail.com>
2021-02-12 00:11:26 +0300

    Update MainScreen.js

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -5,14 +5,17 @@ export const MainScreen = props => {
     return (
         <View>
             <Button title="Войти"/>
-            <Button style={styles.registred} title="Зарегистрироватся"/>
+            <View style={styles.registred}>
+                <Button color="#841584" title="Зарегистрироватся"/>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     registred: {
-        padding: 30,
-        margin: 30
+        margin: 30,
+        height: 100,
+        width: 200
     }
 })
